75b4b099c7a45e1bdb346e6ceaaf351d910b5540
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-10-27 12:56:51 +0530

    minor changes in port and app.js

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var customers = require('./routes/customers');
+var inventory = require('./routes/inventory');
+var history = require('./routes/history');
+var transaction = require('./routes/transaction');
 
 var app = express();
 
@@ -15,6 +19,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,6 +27,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/customers', customers);
+app.use('/inventory', inventory);
+app.use('/history', history);
+app.use('/transaction', transaction);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
